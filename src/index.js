d69aae504d03e70f6ee4ca86aedcb1b2830c7942
progammerBoy <134697099+shubhGlobus@users.noreply.github.com>
2024-01-11 16:31:59 +0530

    utils code add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -10,6 +11,18 @@ dotenv.config(
 );
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running on port: ${process.env.PORT}`);
+    })
+    app.on("error", (err) => {
+        console.log("Error: ", err);
+        throw err;
+    })
+})
+.catch((error) => { 
+    console.log("MONGODB CONNECTION FAILED: ", error);
+} )
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => next(err))
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//     try {
+//         await fn(res, req, next)
+    
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+
+//  }
